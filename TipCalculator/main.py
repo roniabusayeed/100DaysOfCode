f3b884acfa5d17631342535521c8534c89d2e338
roniabusayeed <roni.abusayeed@gmail.com>
2022-01-22 20:06:07 +0600

    Tip calculator

---
+++ TipCalculator/main.py
@@ -0,0 +1,8 @@
+print("Welcome to the tip calculator!")
+total_bill = float(input("What was the total bill? $"))
+tip_percentage = float(input("How much tip would you likfe to give? 10, 12, or 15? ")) / 100
+total_people = int(input("How many people to split the bill? "))
+
+# Calculate payment for each person
+payment_each = total_bill * (1 + tip_percentage) / total_people
+print(f"Each person should pay: ${round(payment_each, 2)}")
