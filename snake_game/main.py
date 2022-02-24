5d3d3e03e06e3db06b3ed3779211fc42a96187f8
roniabusayeed <roni.abusayeed@gmail.com>
2022-02-24 07:36:03 +0600

    Make the snake move forward smoothly.

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,11 +1,12 @@
 from turtle import Turtle, Screen
-
+import time
 
 # Set up the screen.
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 
 # Create snake body using square shaped turtles.
@@ -17,5 +18,18 @@ for i in range(3):
     segment.color("white")
     segment.setx(segment.xcor() - SEGMENT_WIDTH * i)
     snake.append(segment)
+
+
+# Move the snake.
+game_is_on = True
+while game_is_on:
+    screen.update()  # Perform a screen update only when all the segments are moved.
+    time.sleep(.3)  # The make the snake move at a reasonable speed.
+
+    # Make the body segments of the snake follow the ones preceding them.
+    for i in range(len(snake) - 1, 0, -1):
+        snake[i].setposition(snake[i-1].position())
+    snake[0].forward(20)  # Move the head one step forward.
+
 
 screen.exitonclick()
