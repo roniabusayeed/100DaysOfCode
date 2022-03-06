b5f13df31621aba66bfdd3ae32f9f53530d6e446
roniabusayeed <roni.abusayeed@gmail.com>
2022-03-06 11:03:29 +0600

    setup UI

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,4 +1,4 @@
-
+import tkinter
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,32 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Tomato timer.
+canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+image = tkinter.PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=image)
+canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
+canvas.grid(row=1, column=1)
+
+# Timer label.
+timer_label = tkinter.Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+timer_label.grid(row=0, column=1)
+
+# Start button.
+start = tkinter.Button(text="Start")
+start.grid(row=2, column=0)
+
+# Reset button.
+reset = tkinter.Button(text="Reset")
+reset.grid(row=2, column=2)
+
+# Checkmark.
+CHECK_MARK = "✔"
+check_mark = tkinter.Label(text=CHECK_MARK, bg=YELLOW, fg=GREEN, font=(FONT_NAME, 11, "bold"))
+check_mark.grid(row=3, column=1)
+
+window.mainloop()
