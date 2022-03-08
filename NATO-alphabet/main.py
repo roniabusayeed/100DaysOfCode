0a6e4cc70bee9b3f1a06f1de0c5aae73f4d10728
roniabusayeed <roni.abusayeed@gmail.com>
2022-03-08 09:00:00 +0600

    Add exception handling to user input for NATO alphabets

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -2,6 +2,13 @@ import pandas
 
 alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (index, row) in alphabet.iterrows()}
-word = input("Enter a word: ")
-codes = [alphabet_dict[letter] for letter in word.upper()]
+while True:
+    try:
+        word = input("Enter a word: ").upper()
+        codes = [alphabet_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        break
+
 print(codes)
