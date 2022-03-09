d9257ddef0ac090abd688a250afd1d8f902c6cbc
roniabusayeed <roni.abusayeed@gmail.com>
2022-03-09 09:54:16 +0600

    Adjust font size and its positioning

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -2,8 +2,8 @@ import tkinter
 
 # Constants.
 BACKGROUND_COLOR = "#B1DDC6"
-TITLE_FONT = ("Arial", 24, "italic")
-WORD_FONT = ("Arial", 48, "bold")
+TITLE_FONT = ("Arial", 40, "italic")
+WORD_FONT = ("Arial", 60, "bold")
 
 # Setup UI
 window = tkinter.Tk()
@@ -15,7 +15,7 @@ canvas.config(width=800, height=526)  # Make it the same dimensions as the image
 canvas.config(highlightthickness=0, bg=BACKGROUND_COLOR)  # Blend with background.
 image = tkinter.PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=image)  # Place the image at the center of the canvas.
-canvas.create_text(400, 131, text="Title", font=TITLE_FONT)
+canvas.create_text(400, 150, text="Title", font=TITLE_FONT)
 canvas.create_text(400, 263, text="Word", font=WORD_FONT)
 canvas.grid(row=0, column=0, columnspan=2)  # Place the canvas on the window.
 
