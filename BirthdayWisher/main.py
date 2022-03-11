9a39b847644b1c71ea12a5eab2517fed969b8246
roniabusayeed <roni.abusayeed@gmail.com>
2022-03-11 19:09:54 +0600

    pick one email template at random

--- BirthdayWisher/main.py
+++ BirthdayWisher/main.py
@@ -1,22 +1,33 @@
-import smtplib
-import json
+"""
+Sends a birthday email to everyone who has their birthday today.
+Steps:
+1. Read a list of birthdays [(name, email, date of birth),...]
+2. For each of the dates, check if it's today's date.
+3. If it is today's date, "email them".
 
-try:
-    with open("sender_email_creds.json") as file:
-        sender = json.load(file)
-        email = sender["email"]
-        password = sender["password"]
-except FileNotFoundError:
-    # Exit the program.
-    print("Sender credentials file not found")
-    exit()
+Sending an email steps:
+1. create message.
+steps:
+    1. pick a random birthday email template.
+    2. replace the placeholders with their name.
+2. send email:
+steps:
+    1. create email connection.
+    2. activate tls.
+    3. login.
+    4. send email.
+"""
 
-message = "Hello!"
-recipient_email_address = "foo.bar@gmail.com"
+import random
+import glob
+import os
 
-HOST = "smtp.gmail.com"
-connection = smtplib.SMTP(HOST)
-connection.starttls()
-connection.login(user=email, password=password)
-connection.sendmail(from_addr=email, to_addrs=recipient_email_address, msg=message)
-connection.close()
+# Pick a random birthday email template.
+templates = []
+# Iterate over all template files in templates directory.
+for template_filename in glob.glob(os.path.join("templates", "template*.txt")):
+    # Read the content of each template file and add it to the list of templates.
+    with open(template_filename) as template_file:
+        templates.append(template_file.read())
+# Pick one of the templates randomly.
+random_template = random.choice(templates)
